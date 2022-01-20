f8ea6921f333d42f59ece865fc69a3c786de0114
Alex <alpeca2@edem.es>
2022-01-20 18:59:25 +0100

    SQL + Tableau

---
+++ SQL/Advanced SQL Topics/CASE 2.sql
@@ -0,0 +1,15 @@
+SELECT  
+    dm.emp_no,  
+    e.first_name,  
+    e.last_name,  
+    MAX(s.salary) - MIN(s.salary) AS salary_difference,  
+    IF(MAX(s.salary) - MIN(s.salary) > 30000, 'Salary was raised by more then $30,000', 'Salary was NOT raised by more then $30,000') AS salary_increase  
+
+FROM  
+    dept_manager dm  
+	JOIN  
+    employees e ON e.emp_no = dm.emp_no  
+	JOIN  
+    salaries s ON s.emp_no = dm.emp_no  
+
+GROUP BY s.emp_no;
---
+++ SQL/Advanced SQL Topics/Indexes.sql
@@ -0,0 +1,14 @@
+SELECT
+    *
+FROM
+    salaries
+WHERE
+    salary > 89000;
+
+CREATE INDEX i_salary ON salaries(salary);
+SELECT
+    *
+FROM
+    salaries
+WHERE
+    salary > 89000;
